5c0650bebf07b1ca482e13ab92d49c673f1adbaa
Merfoo <fauzimerfoo@gmail.com>
2015-02-04 16:28:56 -0800

    Can get data from thebluealliance with ajax now

--- Code/js/index.js
+++ Code/js/index.js
@@ -12,7 +12,24 @@ function init()
 
 function main()
 {
-    contr.update(navigator.getGamepads()[0]);
+    contr.update(navigator.getGamepads()[0]);    
+    getData("team/frc254/years_participated", print);
     console.log("Main Yo");
-    window.requestAnimationFrame(main);
+    //window.requestAnimationFrame(main);
+}
+
+function print(str)
+{
+    console.log(str);
+}
+
+function getData(key, callback)
+{
+    $.ajax({
+        url:"http://www.thebluealliance.com/api/v2/" + key + "?X-TBA-App-Id=frc955:scouting-system:v01",
+        success:callback,
+        error:function(){
+            callback(null);
+        } 
+    });
 }
