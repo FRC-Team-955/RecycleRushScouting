8175b7e32f9a9c5738e3ce42dcc668cc8f36d037
Merfoo <fauzimerfoo@gmail.com>
2015-02-04 22:33:50 -0800

    Added comments to controller button indexes

--- Code/js/config.js
+++ Code/js/config.js
@@ -1,20 +1,20 @@
 maxTeams = 3;
 maxContrBtns = 16;
 contrBtn = {
-  a: 0,
-  b: 1,
-  x: 2,
-  y: 3,
-  lb: 4,
-  rb: 5,
-  lt: 6,
-  rt: 7,
-  bk: 8,
-  st: 9,
-  ls: 10,
-  rs: 11,
-  du: 12,
-  dd: 13,
-  dl: 14,
-  dr: 15
+  a: 0,     // a
+  b: 1,     // b
+  x: 2,     // x
+  y: 3,     // y
+  lb: 4,    // left bumper
+  rb: 5,    // right bumper
+  lt: 6,    // left trigger
+  rt: 7,    // right trigger
+  bk: 8,    // back
+  st: 9,    // start
+  ls: 10,   // left stick
+  rs: 11,   // right stick
+  du: 12,   // dpad up
+  dd: 13,   // dpad down
+  dl: 14,   // dpad left
+  dr: 15    // dpad right
 };
