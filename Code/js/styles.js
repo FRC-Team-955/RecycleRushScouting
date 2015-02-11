1f8d781eb068e013cb4f4484e0299d7073538eed
Merfoo <fauzimerfoo@gmail.com>
2015-02-11 10:46:19 -0800

    User can change buttons in tags area now

--- Code/js/styles.js
+++ Code/js/styles.js
@@ -1,22 +1,89 @@
 function initStyle() {
 	setElements();
 	updateGui();
-	$(".pure-button").click(function() {
-		$("#" + this.id).toggleClass("selected");
-	});
+	
+	for(var subProp in cssButtonAreaName.tags)
+		$("." + cssButtonAreaName.tags[subProp]).click(function() { tagButtonClick(this.id); });
 }
 
 function updateGui()
 {
+	// Reset all the buttons gui
 	for(var prop in cssButtonAreaName)
 		for(var subProp in cssButtonAreaName[prop])
 			$("." + cssButtonAreaName[prop][subProp]).removeClass(cssButtonActiveName + " " + cssButtonFocusedName);
+	
+	// Index for button in array of button elements in gui obj
+	var btnIndex = 0;
+
+	// Set buttons that are active gui wise for tags
+	for(var subProp in alliance[curTeamIndex].data.tags)
+	{
+		btnIndex = 0;
+
+		for(var innerProp in alliance[curTeamIndex].data.tags[subProp])
+		{	
+			if(alliance[curTeamIndex].data.tags[subProp][innerProp] === 1)
+				$gui.tags[subProp][btnIndex].addClass(cssButtonActiveName);
+				
+			btnIndex++;
+		}
+	}
+	
+	// Set the buttons text to match robot data for scoring
+	for(var subProp in alliance[curTeamIndex].data.scoring)
+	{
+		btnIndex = 0;
+		
+		for(var innerProp in alliance[curTeamIndex].data.scoring[subProp])
+		{
+			// TODO: Remove this try/catch when these elements actually exist in html...
+			try
+			{
+				$gui.scoring[subProp][btnIndex++].html(alliance[curTeamIndex].data.scoring[subProp][innerProp]);
+			}
+			
+			catch(e)
+			{
+				console.log(e);
+			}
+		}
+	}
 	
 	var tagBtnId = getTagInnerIndex();
 	var tagSubName = tagsNames[currMode.tags.subId];
 	var scoringSubName = scoringNames[currMode.scoring.subId];
 	$gui.tags[tagSubName][tagBtnId].addClass(cssButtonFocusedName);
 	$gui.scoring[scoringSubName][currMode.scoring.innerId].addClass(cssButtonFocusedName);
+}
+
+function tagButtonClick(elmName)
+{
+	var $elm = $("#" + elmName);
+	
+	// Find button in gui and alliance data
+	for(var subProp in cssButtonAreaName.tags)
+	{
+		var btnIndex = 0;
+		
+		if($elm.hasClass(cssButtonAreaName.tags[subProp]))
+		{
+			for(var innerProp in tagsInnerNames[subProp])
+			{
+				// Flip alliance tag data
+				if(elmName === $gui.tags[subProp][btnIndex][0].id)
+				{
+					var innerName = tagsInnerNames[subProp][btnIndex];
+					var currVal = alliance[curTeamIndex].data.tags[subProp][innerName];
+					alliance[curTeamIndex].data.tags[subProp][innerName] = (currVal === 0 ? 1 : 0);
+				}
+				
+				btnIndex++;
+			}
+		}
+	}
+	
+	updateGui();
 }
 
 function setElements()
